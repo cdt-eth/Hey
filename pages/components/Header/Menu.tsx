849cce5c359c45d57cc42e9aec6ca09c961e1874
christianturner <christianturner@hey.com>
2021-08-26 15:08:08 -0400

    add down arrow icon

--- pages/components/Header/Menu.tsx
+++ pages/components/Header/Menu.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import { IoIosArrowDown as ArrowDown } from "react-icons/io";
 
 const Menu = () => {
   return (
-    <div className="flex flex-1 justify-center">
+    <div className="flex flex-1 justify-center items-center">
       <Image src="/logo.svg" alt="hey-logo" width={85} height={40} />
+      <ArrowDown className="text-bright text-2xl " />
     </div>
   );
 };
