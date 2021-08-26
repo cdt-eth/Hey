972021dc82b989c36b68e0d044b78e47502d6e71
christianturner <christianturner@hey.com>
2021-08-26 15:18:42 -0400

    search box icon

--- pages/components/Header/Search.tsx
+++ pages/components/Header/Search.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { IoSearch as SearchIcon } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className="flex-1">
+    <div className="flex-1 flex items-center relative">
+      <SearchIcon className="text-light-grey text-3xl absolute pl-2" />
       <input
         placeholder="Search"
-        className="bg-light text-light-grey text-lg font-semibold rounded-full placeholder-light-grey px-2 py-2"
+        className="bg-light outline-none	 text-light-grey text-xl font-semibold rounded-full placeholder-light-grey px-2 py-2 pl-10"
       />
     </div>
   );
