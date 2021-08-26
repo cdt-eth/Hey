e0cb4e77b2ed99c7d29490d5b4066e7c1c506844
christianturner <christianturner@hey.com>
2021-08-26 15:18:32 -0400

    header padding

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import User from "./User";
 
 const Header = () => {
   return (
-    <div className="px-7 py-2 flex items-stretch">
+    <div className="px-24 py-2 flex items-stretch">
       <Search />
       <Menu />
       <User />
