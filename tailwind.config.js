613d6f18dc4e977a475d67f75c339141fbfaae2f
christianturner <christianturner@hey.com>
2021-08-26 07:43:40 -0400

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      dark: "#1a2733",
+      mid: "#212C38",
+      light: "#27323E",
+      bright: "#50A2FF",
+      "light-grey": "#9DFFEB",
+      "dark-grey": "#9CA0A2",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
