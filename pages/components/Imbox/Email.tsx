91e1edc58f700ef6e9967736fea12dba13f4e2ed
christianturner <christianturner@hey.com>
2021-08-27 10:25:37 -0400

    responsive truncate text and image prop

--- pages/components/Imbox/Email.tsx
+++ pages/components/Imbox/Email.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 interface Email {
   subject: string;
   body: string;
+  url: string;
 }
 
 const today = new Date();
@@ -17,20 +18,22 @@ const now = today
   .replace("PM", "pm")
   .replace(" ", "");
 
-const Email = ({ subject, body }: Email) => {
+const Email = ({ subject, body, url }: Email) => {
   return (
-    <div className="flex">
+    <div className="flex mb-5">
       <p className="text-unread self-center text-5xl">•</p>
       <Image
-        className="rounded-full self-center"
-        src="/user.jpg"
+        className="rounded-full self-center flex-1"
+        src={`/${url}`}
         alt="me"
         width={50}
         height={50}
       />
-      <div className="self-center px-2">
-        <p className="text-xl font-bold text-off-white">{subject}</p>
-        <p className="text-light-grey">{body}</p>
+      <div className="self-center px-2 max-w-full	w-5/6">
+        <p className="text-xl font-bold text-off-white truncate w-auto">
+          {subject}
+        </p>
+        <p className="text-light-grey w-auto truncate">{body}</p>
       </div>
 
       <p className="text-light-grey text-lg flex flex-1 items-center justify-end ">
