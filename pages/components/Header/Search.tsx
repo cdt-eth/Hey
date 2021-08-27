533e20ce720fd56009fca57d64d7003aee69ecd1
christianturner <christianturner@hey.com>
2021-08-26 21:18:21 -0400

    search font styles

--- pages/components/Header/Search.tsx
+++ pages/components/Header/Search.tsx
@@ -7,7 +7,7 @@ const Search = () => {
       <SearchIcon className="text-light-grey text-3xl absolute pl-2" />
       <input
         placeholder="Search"
-        className="bg-light outline-none	 text-light-grey text-xl font-semibold rounded-full placeholder-light-grey px-2 py-2 pl-10"
+        className="bg-light outline-none text-light-grey text-xl font-semibold rounded-full placeholder-light-grey px-2 py-2 pl-10"
       />
     </div>
   );
