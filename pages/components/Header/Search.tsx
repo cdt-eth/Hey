fe69fb83e08fbf54444222aa972ca461fa77dd99
christianturner <christianturner@hey.com>
2021-08-27 09:50:42 -0400

    new color

--- pages/components/Header/Search.tsx
+++ pages/components/Header/Search.tsx
@@ -4,10 +4,10 @@ import { IoSearch as SearchIcon } from "react-icons/io5";
 const Search = () => {
   return (
     <div className="flex-1 flex items-center relative">
-      <SearchIcon className="text-light-grey text-3xl absolute pl-2" />
+      <SearchIcon className="text-off-white text-3xl absolute pl-2" />
       <input
         placeholder="Search"
-        className="bg-light outline-none text-light-grey text-xl font-semibold rounded-full placeholder-light-grey px-2 py-2 pl-10"
+        className="bg-light outline-none text-off-white text-xl font-semibold rounded-full placeholder-off-white px-2 py-2 pl-10"
       />
     </div>
   );
--- pages/components/Header/User.tsx
+++ pages/components/Header/User.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const User = () => {
   return (
     <div className="flex items-center justify-end flex-1 pr-5">
-      <p className="text-light-grey font-md text-xl px-2">Me</p>
+      <p className="text-off-white font-md text-xl px-2">Me</p>
       <Image
         className="rounded-full"
         src="/user.jpg"
