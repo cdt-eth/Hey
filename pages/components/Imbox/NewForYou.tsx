124d9d541455af030594e769cdae5b5a1fdf69ee
christianturner <christianturner@hey.com>
2021-08-30 09:23:21 -0400

    mobile UI

--- pages/components/Imbox/NewForYou.tsx
+++ pages/components/Imbox/NewForYou.tsx
@@ -1,9 +1,11 @@
 const NewForYou = () => {
   return (
-    <div className="flex px-14 items-center py-5">
-      <p className="flex-none text-off-white text-md font-bold ">NEW FOR YOU</p>
-      <hr className="flex-grow text-bright mx-2" />
-      <p className="flex-none text-bright tracking-wide flex justify-end">
+    <div className="flex xs:px-4 sm:px-14 items-center py-5 xs:justify-between">
+      <p className="flex-none text-off-white xs:text-xs sm:text-md font-bold ">
+        NEW FOR YOU
+      </p>
+      <hr className="flex-grow text-bright mx-2 xs:hidden sm:flex" />
+      <p className="flex-none text-bright tracking-wide flex justify-end xs: text-xs sm:text-sm">
         Read Together
       </p>
     </div>
