757b4cf42639737a9641860d7b59b7e7b816f546
christianturner <christianturner@hey.com>
2021-08-30 09:23:56 -0400

    mobile UI

--- pages/components/Imbox/Imbox.tsx
+++ pages/components/Imbox/Imbox.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Email from "./Email";
+// import Email from "./Email";
 import NewForYou from "./NewForYou";
 import Read from "./Read";
 import Unread from "./Unread";
@@ -8,15 +8,15 @@ import Write from "./Write";
 
 const Imbox = () => {
   return (
-    <div className="lg:w-5/6  md:w-full m-auto px-16">
-      <div className="bg-light rounded-t-3xl pt-6">
+    <div className="md:w-full m-auto  sm:px-5 lg:x-5 xl:w-5/6">
+      <div className="xs:bg-mid sm:bg-light sm:rounded-t-3xl pt-6">
         <div className="flex justify-between items-center mx-6">
           <Screener />
 
           <Write />
         </div>
 
-        <h2 className="text-off-white m-auto text-center text-5xl font-bold">
+        <h2 className="text-off-white m-auto text-center sm:text-3xl lg:text-5xl font-bold">
           Imbox
         </h2>
 
