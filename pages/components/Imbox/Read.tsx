37a4580a3eb13b69794199506fbcd02e0176f2b9
christianturner <christianturner@hey.com>
2021-08-30 09:23:40 -0400

    mobile UI

--- pages/components/Imbox/Read.tsx
+++ pages/components/Imbox/Read.tsx
@@ -3,9 +3,9 @@ import Email from "./Email";
 
 const Read = () => {
   return (
-    <div className="bg-mid pb-4">
-      <div className="px-14">
-        <div className="pb-5 pt-8 text-off-white font-bold">
+    <div className="xs:bg-light sm:bg-mid pb-4">
+      <div className="xs:px-2 sm:px-14">
+        <div className="pb-5 pt-8 text-off-white font-bold xs:text-xs sm:text-sm">
           PREVIOUSLY SEEN
         </div>
         <Email
@@ -14,6 +14,7 @@ const Read = () => {
             "CS 6035 via Ed - Your instructor Christian Turner posted an announcement on Ed. Simply click to"
           }
           url="user.jpg"
+          read={true}
         />
         <Email
           subject={
@@ -23,6 +24,7 @@ const Read = () => {
             "vercel[bot] - Successfully deployed to the following URLs: • hey-clone.vercel.app • hey-christiandavidturner.ve"
           }
           url="github.png"
+          read={true}
         />
         <Email
           subject={"Square just requested your Coding Report!"}
@@ -30,6 +32,7 @@ const Read = () => {
             "CodeSignal - Sign in to take the test and share your Coding Report with Square . Square is waiting for your"
           }
           url="user.jpg"
+          read={true}
         />
         <Email
           subject={"The Design Event: Our tips & tricks for a home refresh"}
@@ -37,6 +40,7 @@ const Read = () => {
             "Crate & Barrel - Furniture, Home Decor and Wedding Registry | Crate and Barrel Plus, earn Double Rewards through"
           }
           url="user.jpg"
+          read={true}
         />
         <Email
           subject={
@@ -46,6 +50,7 @@ const Read = () => {
             "Tanner Murray via DocuSign - Tanner Murray sent you a document to review and sign. REVIEW DOCUMENT Tanner Murray tmurray@"
           }
           url="user.jpg"
+          read={true}
         />
       </div>
     </div>
--- pages/components/Imbox/Unread.tsx
+++ pages/components/Imbox/Unread.tsx
@@ -3,13 +3,14 @@ import Email from "./Email";
 
 const Unread = () => {
   return (
-    <div className="px-14">
+    <div className="xs:px-2 sm:px-14">
       <Email
         subject={"CS 6035 - Project 1 - Part 1 Released!"}
         body={
           "CS 6035 via Ed - Your instructor Christian Turner posted an announcement on Ed. Simply click to"
         }
         url="user.jpg"
+        read={false}
       />
       <Email
         subject={
@@ -19,6 +20,7 @@ const Unread = () => {
           "vercel[bot] - Successfully deployed to the following URLs: • hey-clone.vercel.app • hey-christiandavidturner.ve"
         }
         url="github.png"
+        read={false}
       />
       <Email
         subject={"Square just requested your Coding Report!"}
@@ -26,6 +28,7 @@ const Unread = () => {
           "CodeSignal - Sign in to take the test and share your Coding Report with Square . Square is waiting for your"
         }
         url="user.jpg"
+        read={false}
       />
       <Email
         subject={"The Design Event: Our tips & tricks for a home refresh"}
@@ -33,6 +36,7 @@ const Unread = () => {
           "Crate & Barrel - Furniture, Home Decor and Wedding Registry | Crate and Barrel Plus, earn Double Rewards through"
         }
         url="user.jpg"
+        read={false}
       />
       <Email
         subject={
@@ -42,6 +46,7 @@ const Unread = () => {
           "Tanner Murray via DocuSign - Tanner Murray sent you a document to review and sign. REVIEW DOCUMENT Tanner Murray tmurray@"
         }
         url="user.jpg"
+        read={false}
       />
     </div>
   );
