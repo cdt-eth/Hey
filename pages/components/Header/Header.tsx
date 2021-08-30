1198278786040c5b5b2003d4f2741db1e59cec9b
christianturner <christianturner@hey.com>
2021-08-30 12:48:21 -0400

    search component mobile fix

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -5,10 +5,33 @@ import User from "./User";
 
 const Header = () => {
   return (
-    <div className="lg:px-24 xs:px-4 md:w-full xs:pb-px xs:pt-2 sm:py-2 px-2 sm:pb-10 m-auto flex items-stretch xs:bg-light sm:bg-dark">
-      <Search />
-      <Menu />
-      <User />
+    <div
+      className="
+        xs:px-4
+        xs:pb-px
+        xs:pt-2
+      xs:bg-light
+      sm:bg-dark
+        sm:py-2
+        sm:pb-10
+        lg:px-24
+        md:w-full
+        px-2
+        m-auto
+        flex
+        items-stretch
+        justify-between
+        "
+    >
+      <div className="third one xs:self-center">
+        <Search />
+      </div>
+      <div className="third">
+        <Menu />
+      </div>
+      <div className="third">
+        <User />
+      </div>
     </div>
   );
 };
--- pages/components/Header/Search.tsx
+++ pages/components/Header/Search.tsx
@@ -3,11 +3,28 @@ import { IoSearch as SearchIcon } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className="sm:flex-1 flex xs:flex-grow items-center relative">
+    <div className="flex items-center relative test sm:w-48">
       <SearchIcon className="xs:text-light-grey sm:text-off-white xs:text-2xl sm:text-3xl absolute pl-2" />
       <input
         placeholder="Search"
-        className="xs:bg-dark sm:bg-light outline-none text-off-white xs:text-sm sm:text-xl font-semibold rounded-full xs:placeholder-light-grey sm:placeholder-off-white xs:flex-grow xs:mr-2 px-2 py-2 xs:pl-8 sm:pl-10"
+        className="
+        xs:bg-dark
+          xs:text-sm
+        xs:placeholder-light-grey
+          xs:flex-grow
+          xs:mr-2
+          xs:pl-8
+        sm:bg-light
+          sm:text-xl
+          sm:pl-10
+        sm:placeholder-off-white
+          font-semibold
+          outline-none
+        text-off-white
+          rounded-full
+          px-2
+          py-2
+        "
       />
     </div>
   );
