40a8f77e4769f2e1f3b3df4d5cccfb3544f4633c
christianturner <christianturner@hey.com>
2021-08-31 06:29:31 -0400

    next/link for clickable Hey logo

--- pages/components/Header/Menu.tsx
+++ pages/components/Header/Menu.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import { IoIosArrowDown as ArrowDown } from "react-icons/io";
+import Link from "next/link";
 
 const Menu = () => {
   return (
-    <div className="xs:hidden sm:flex flex-1 justify-center items-center">
-      <Image src="/logo.svg" alt="hey-logo" width={85} height={40} />
-      <ArrowDown className="text-bright text-2xl " />
-    </div>
+    <Link href="#" passHref>
+      <div className="cursor-pointer xs:hidden sm:flex flex-1 justify-center items-center">
+        <Image src="/logo.svg" alt="hey-logo" width={85} height={40} />
+        <ArrowDown className="text-bright text-2xl " />
+      </div>
+    </Link>
   );
 };
 
