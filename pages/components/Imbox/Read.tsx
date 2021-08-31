0b1cb7bb98030e95af6de3297992dea99800b4d1
christianturner <christianturner@hey.com>
2021-08-31 06:22:49 -0400

    album icon button

--- pages/components/Imbox/Read.tsx
+++ pages/components/Imbox/Read.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import Email from "./Email";
+import Image from "next/image";
+import Link from "next/link";
 
 const Read = () => {
   return (
     <div className="xs:bg-light sm:bg-mid pb-4">
       <div className="xs:px-2 sm:px-14">
-        <div className="pb-5 pt-6 text-off-white font-bold xs:text-xs sm:text-sm">
-          PREVIOUSLY SEEN
+        <div className="flex justify-between items-center">
+          {/*
+          
+          */}
+
+          <div className="pb-5 pt-6 text-off-white font-bold xs:text-xs sm:text-sm">
+            PREVIOUSLY SEEN
+          </div>
+
+          <Link href="#" passHref>
+            <Image
+              className="justify-end cursor-pointer"
+              alt="album"
+              src="/album.svg"
+              height={22}
+              width={22}
+            />
+          </Link>
         </div>
+
         <Email
           subject={"CS 6035 - Project 1 - Part 1 Released!"}
           body={
