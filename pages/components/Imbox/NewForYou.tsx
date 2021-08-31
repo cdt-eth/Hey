1593edd884ac1fc2e1e4c519fe45d96ccabbbd0e
christianturner <christianturner@hey.com>
2021-08-31 06:27:04 -0400

    next/link for 'Read Togeter' CTA

--- pages/components/Imbox/NewForYou.tsx
+++ pages/components/Imbox/NewForYou.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
+
 const NewForYou = () => {
   return (
     <div className="flex xs:px-4 sm:px-14 items-center py-5 xs:justify-between">
       <p className="flex-none text-off-white xs:text-xs sm:text-md font-bold ">
         NEW FOR YOU
       </p>
+
       <hr className="flex-grow text-bright mx-2 xs:hidden sm:flex" />
-      <p className="flex-none text-bright tracking-wide flex justify-end xs: text-xs sm:text-sm">
-        Read Together
-      </p>
+
+      <Link href="#" passHref>
+        <p className="cursor-pointer flex-none text-bright tracking-wide flex justify-end xs: text-xs sm:text-sm">
+          Read Together
+        </p>
+      </Link>
     </div>
   );
 };
