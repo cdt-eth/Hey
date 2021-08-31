0c2bcd813e9958333a48fbb0f3dfb588a95d9ff6
christianturner <christianturner@hey.com>
2021-08-31 06:29:46 -0400

    next/link for clickable User icon

--- pages/components/Header/User.tsx
+++ pages/components/Header/User.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const User = () => {
   return (
-    <div className="flex items-center justify-end sm:flex-1 sm:pr-5">
-      <p className="text-off-white font-md text-xl px-2 xs:hidden sm:flex">
-        Me
-      </p>
-      <Image
-        className="rounded-full"
-        src="/user.jpg"
-        alt="me"
-        width={43}
-        height={43}
-      />
-    </div>
+    <Link href="#" passHref>
+      <div className="cursor-pointer flex items-center justify-end sm:flex-1 sm:pr-5">
+        <p className="text-off-white font-md text-xl px-2 xs:hidden sm:flex">
+          Me
+        </p>
+        <Image
+          className="rounded-full"
+          src="/user.jpg"
+          alt="me"
+          width={43}
+          height={43}
+        />
+      </div>
+    </Link>
   );
 };
 
