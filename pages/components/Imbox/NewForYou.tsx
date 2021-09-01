c05f5154201ded105f66ad3ff9f491c9214869b1
christianturner <christianturner@hey.com>
2021-08-31 22:22:35 -0400

    inbox zero change to NewForYou component conditional

--- pages/components/Imbox/NewForYou.tsx
+++ pages/components/Imbox/NewForYou.tsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
+import { useState } from "react";
 
 const NewForYou = () => {
+  const [inboxZero, setInboxZero] = useState(false);
+
   return (
     <div className="flex xs:px-4 sm:px-14 items-center py-5 xs:justify-between">
-      <p className="flex-none text-off-white xs:text-xs sm:text-md font-bold ">
-        NEW FOR YOU
-      </p>
+      {inboxZero && (
+        <p className="flex-none text-off-white xs:text-xs sm:text-md font-bold ">
+          NEW FOR YOU
+        </p>
+      )}
 
       <hr className="flex-grow text-bright mx-2 xs:hidden sm:flex" />
 
-      <Link href="#" passHref>
-        <p className="cursor-pointer flex-none text-bright tracking-wide flex justify-end xs: text-xs sm:text-sm">
-          Read Together
-        </p>
-      </Link>
+      {inboxZero && (
+        <Link href="#" passHref>
+          <p className="cursor-pointer flex-none text-bright tracking-wide flex justify-end xs: text-xs sm:text-sm">
+            Read Together
+          </p>
+        </Link>
+      )}
     </div>
   );
 };
